ec62cc2b2a08b20702c37a34269d5a99e18afc1e
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-16 09:41:05 -0600

    test for linking files

--- __test__/solar-age.test.js
+++ __test__/solar-age.test.js
@@ -1,19 +1,38 @@
 import Triangle from '../src/solar-age.js';
 
-describe('solar-age', () => {
+beforeEach('solar-age', () => {
 
-  test('should correctly create a triangle object with three lengths', () => {
-    const triangle = new Triangle(2, 4, 5);
-    expect(triangle.side1).toEqual(2);
-    expect(triangle.side2).toEqual(4);
-    expect(triangle.side3).toEqual(5);
+
+
+
+  test('should link files correctly', () => {
   });
-  test('should correctly determine whether three lengths are not a triangle', () => {
-    const notTriangle = new Triangle(3, 9, 22);
-    expect(notTriangle.checkType()).toEqual("not a triangle");
-  });
-  test('should correctly determine whether three lengths make a scalene triangle', () => {
-    const scalTriangle = new Triangle(4, 5, 7)
-    expect(scalTriangle.checkType()).toEqual("scalene triangle");
-  });
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+  
+  // const triangle = new Triangle(2, 4, 5);
+  // expect(triangle.side1).toEqual(2);
+  // expect(triangle.side2).toEqual(4);
+  // expect(triangle.side3).toEqual(5);
+
+  // test('should correctly determine whether three lengths are not a triangle', () => {
+  //   const notTriangle = new Triangle(3, 9, 22);
+  //   expect(notTriangle.checkType()).toEqual("not a triangle");
+  // });
+  // test('should correctly determine whether three lengths make a scalene triangle', () => {
+  //   const scalTriangle = new Triangle(4, 5, 7)
+  //   expect(scalTriangle.checkType()).toEqual("scalene triangle");
+  // });
 });
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './css/styles.css';
 import solar-age from './solar-age.js';
 
 
